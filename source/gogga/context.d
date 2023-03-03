6322b1db7709284eadb7cfa144d3ed5c4d9f7fb0
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-03-03 11:30:56 +0200

    Context
    - Added more documentation

--- source/gogga/context.d
+++ source/gogga/context.d
@@ -1,7 +1,13 @@
+/** 
+ * Custom context for gogga logging
+ */
 module gogga.context;
 
 import dlog;
 
+/** 
+ * Pass in a custom context to be used for gogga
+ */
 public final class GoggaContext : Context
 {
     // TODO: Put more advanced stuff here
