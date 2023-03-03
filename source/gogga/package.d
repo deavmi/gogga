98b65305a5392947bd4a60a8fafb6fcd9d00a025
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-03-03 11:30:26 +0200

    Package
    - Added missing documentation

--- source/gogga/package.d
+++ source/gogga/package.d
@@ -3,5 +3,14 @@
  */
 module gogga;
 
+/** 
+ * The logging class which provides the logging print
+ * calls, controlling of style and whether to debug or
+ * not
+ */
 public import gogga.core : GoggaLogger;
+
+/** 
+ * The gogga styles supported
+ */
 public import gogga.transform : GoggaMode;
